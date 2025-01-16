9e78725bf5b6ec10c3c95b3565658198473cd637
HjRu94 <hjalmar@rusck.se>
2025-01-16 13:35:17 +0100

    feat: change message to json

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 SERVER_IP = '101.101.1.2'
@@ -7,16 +8,17 @@ try:
     # Create a socket connection
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((SERVER_IP, SERVER_PORT))
-        print("Connected to the server. Listening for messages...\n")
+        print('Connected to the server. Listening for messages...\n')
 
         while True:
             # Receive and print data from the server
-            data = client.recv(1024).decode("utf-8")
+            data = client.recv(1024).decode('utf-8')
+            data = json.loads(data)
             if data:
-                print(f"Received: {data}")
+                print(f'Received: {data}')
             else:
                 break
 except KeyboardInterrupt:
-    print("\nDisconnected from the server.")
+    print('\nDisconnected from the server.')
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f'An error occurred: {e}')
--- server/server.py
+++ server/server.py
@@ -1,41 +1,49 @@
-import RPi.GPIO as GPIO
+import json
 import socket
 import threading
 import time
 
+import RPi.GPIO as GPIO
+
 # GPIO setup
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
 GPIO.setwarnings(False)
-GPIO_PIN = 17
-GPIO.setup(GPIO_PIN, GPIO.IN)
+GPIO_PINS = [17, 27, 22]
+for pin in GPIO_PINS:
+    GPIO.setup(pin, GPIO.IN)
 
 # Server setup
-HOST = "0.0.0.0"  # Listen on all interfaces
+HOST = '0.0.0.0'  # Listen on all interfaces
 PORT = 12345  # Define your desired port
 
 clients = []
 
+
 def handle_client(conn, addr):
-    print(f"Client connected: {addr}")
+    print(f'Client connected: {addr}')
     try:
         while True:
             data = conn.recv(1024)  # Keep the connection open
             if not data:
                 break
     except Exception as e:
-        print(f"Connection error with {addr}: {e}")
+        print(f'Connection error with {addr}: {e}')
     finally:
-        print(f"Client disconnected: {addr}")
+        print(f'Client disconnected: {addr}')
         clients.remove(conn)
+
         conn.close()
 
+
 # Function to broadcast messages to all clients
-def broadcast(message):
+def broadcast(data):
+    message = json.dumps(data)
     for conn in clients:
         try:
-            conn.sendall(message.encode("utf-8"))
+            conn.sendall(message.encode('utf-8'))
         except Exception as e:
-            print(f"Failed to send message to a client: {e}")
+            print(f'Failed to send message to a client: {e}')
+
 
 # Start the server in a separate thread
 def start_server():
@@ -43,35 +51,37 @@ def start_server():
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind((HOST, PORT))
         server.listen(5)
-        print(f"Server started on {HOST}:{PORT}")
-
+        print(f'Server started on {HOST}:{PORT}')
         while True:
             conn, addr = server.accept()
             clients.append(conn)
             threading.Thread(target=handle_client, args=(conn, addr)).start()
 
+
 # GPIO monitoring function
 def monitor_gpio():
-    print("Monitoring GPIO pin 17 for state changes...")
-    last_state = GPIO.input(GPIO_PIN)
+    print('Monitoring GPIO pins for state changes...')
+    last_states = {pin: GPIO.input(pin) for pin in GPIO_PINS}
 
     while True:
-        current_state = GPIO.input(GPIO_PIN)
-        if current_state != last_state:
-            state_str = "HIGH" if current_state else "LOW"
-            message = f"GPIO pin 17 changed to {state_str}"
-            print(message)
-            broadcast(message)  # Broadcast the state change to clients
-            last_state = current_state
-        time.sleep(0.1)  # Poll every 100ms
+        for pin in GPIO_PINS:
+            current_state = GPIO.input(pin)
+            if current_state != last_states[pin]:
+                message = {'pin': pin, 'state': current_state}
+                print(message)
+                broadcast(message)  # Broadcast the state change to clients
+                last_states[pin] = current_state
+        time.sleep(0.01)  # Poll every 100ms
+
 
 # Main function
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
-        threading.Thread(target=start_server, daemon=True).start()  # Start server
+        # Start server
+        threading.Thread(target=start_server, daemon=True).start()
         monitor_gpio()  # Monitor GPIO
     except KeyboardInterrupt:
-        print("\nExiting gracefully. Cleaning up GPIO...")
+        print('\nExiting gracefully. Cleaning up GPIO...')
         GPIO.cleanup()
         for client in clients:
             client.close()
